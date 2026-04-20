4e1f6f3709913e13a1a3f51353ae1a48377c0f79
AllanGaBRs <allanmediogames@gmail.com>
2026-04-19 22:11:32 -0300

    feat: implements workspace (model and repository)

---
+++ src/main/java/com/allan/task/manager/workspace/WorkspaceModel.java
@@ -0,0 +1,33 @@
+package com.allan.task.manager.workspace;
+
+import com.allan.task.manager.shared.Auditable;
+import com.allan.task.manager.user.UserModel;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_workspace")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WorkspaceModel extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String slug;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private UserModel owner;
+
+}
