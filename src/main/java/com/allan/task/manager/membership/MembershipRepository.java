edaf0d9fc698db6ec6591dfab84056540324fe67
AllanGaBRs <allanmediogames@gmail.com>
2026-05-04 17:15:57 -0300

    refactor(membership): update queries to support active workspace filtering

--- src/main/java/com/allan/task/manager/membership/MembershipRepository.java
+++ src/main/java/com/allan/task/manager/membership/MembershipRepository.java
@@ -1,11 +1,39 @@
 package com.allan.task.manager.membership;
 
-import com.allan.task.manager.checklistItem.ChecklistItemModel;
+import com.allan.task.manager.workspace.WorkspaceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface MembershipRepository extends JpaRepository<MembershipModel, UUID> {
+
+    boolean existsByWorkspaceIdAndUserId(UUID workspaceId, UUID userId);
+
+    Optional<MembershipModel> findByWorkspaceIdAndUserId(UUID workspaceId, UUID userId);
+
+    List<MembershipModel> findByUserId(UUID userId);
+
+    @Query("""
+            SELECT m.workspace
+            FROM MembershipModel m
+            WHERE m.user.id = :userId
+            AND m.workspace.isActive = true
+            """)
+    List<WorkspaceModel> findActiveWorkspacesByUserId(UUID userId);
+
+    @Query("""
+            SELECT m
+            FROM MembershipModel m
+            JOIN FETCH m.workspace w
+            WHERE m.user.id = :userId
+            AND w.isActive = true
+            """)
+    List<MembershipModel> findActiveMembershipsByUserId(UUID userId);
+
+    boolean existsByUserIdAndWorkspaceId(UUID userId, UUID workspaceId);
+
+    List<MembershipModel> findByWorkspaceId(UUID workspaceId);
 }
