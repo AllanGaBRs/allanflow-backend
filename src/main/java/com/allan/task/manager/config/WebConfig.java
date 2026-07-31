cc9ee32442ecce1462242c8c70b5ae93455b4d55
AllanGaBRs <allanmediogames@gmail.com>
2026-07-31 10:39:27 -0300

    refactor(auth): add current user id annotation

---
+++ src/main/java/com/allan/task/manager/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.allan.task.manager.config;
+
+import com.allan.task.manager.config.annotation.resolver.CurrentUserIdArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final CurrentUserIdArgumentResolver currentUserIdArgumentResolver;
+
+    public WebConfig(CurrentUserIdArgumentResolver currentUserIdArgumentResolver) {
+        this.currentUserIdArgumentResolver = currentUserIdArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> resolvers
+    ) {
+        resolvers.add(currentUserIdArgumentResolver);
+    }
+}
